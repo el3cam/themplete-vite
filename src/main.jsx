d2f4329e148e379828ee6b5af84f27e9d8fa9439
el3cam <3.campos.3@gmail.com>
2024-08-12 12:34:50 -0700

    del strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,6 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<QueryClientProvider client={queryClient}>
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>
+		<App />
 	</QueryClientProvider>
 );
